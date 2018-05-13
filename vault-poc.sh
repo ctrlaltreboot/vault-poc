5e7cec1c0b65b2dbf7ae7297480da89573f4758c
ctrlaltreboot <robot@rebooter.xyz>
2018-05-13 23:10:25 +1000

    Refactoring... Cleaning...

--- vault-poc.sh
+++ vault-poc.sh
@@ -4,28 +4,25 @@
 set -e
 
 # autounseal
-: ${AUTOUNSEAL:='no'}
+: ${AUTO_UNSEAL:='no'}
 # autologin
-: ${AUTOLOGIN:='no'}
+: ${AUTO_LOGIN:='no'}
 
 # process id definition
-PIDDIR=$(pwd)/pids
-CONSULPID=$PIDDIR/consul.pid
-VAULTPID=$PIDDIR/vault.pid
+PID_DIR=$(pwd)/pids
+CONSUL_PID="$PID_DIR"/consul.pid
+VAULT_PID="$PID_DIR"/vault.pid
 
 # log directory definition
-LOGDIR=$(pwd)/logs
+LOG_DIR=$(pwd)/logs
 
 # policy directory definition
-POLICYDIR=$(pwd)/policies
+POLICY_DIR=$(pwd)/policies
 
-# policy files definition
-ADMINPOLICY=$POLICYDIR/admin-policy.hcl
-N00BSPOLICY=$POLICYDIR/n00bs-policy.hcl
 
 # create directories if they're missing
-[[ ! -e $PIDDIR ]] && mkdir -pv $PIDDIR
-[[ ! -e $LOGDIR ]] && mkdir -pv $LOGDIR
+[[ ! -e "$PID_DIR" ]] && mkdir -pv "$PID_DIR"
+[[ ! -e "$LOG_DIR" ]] && mkdir -pv "$LOG_DIR"
 
 # define where the operator initialization information
 # would be saved unto...
@@ -39,12 +36,12 @@ start_consul_dev() {
   # start a consule dev server
   # pushing all output to a log file
   # and writing the process id (represented by $!) into a file
-  consul agent -dev &> $LOGDIR/consul.dev.log & echo $! > $CONSULPID
+  consul agent -dev &> "$LOG_DIR"/consul.dev.log & echo $! > "$CONSUL_PID"
 }
 
 start_vault() {
   echo 'Starting Vault Server'
-  vault server -config=vaultconfig.hcl &> $LOGDIR/vault.poc.log & echo $! > $VAULTPID
+  vault server -config=vaultconfig.hcl &> "$LOG_DIR"/vault.poc.log & echo $! > "$VAULT_PID"
 }
 
 init_vault() {
@@ -58,12 +55,12 @@ set_secrets() {
   # the information is then parsed and saved into variable corresponding
   # to the 5 unseal keys and the root token
   if [[ -e $SECRET ]] && [[ -s $SECRET ]]; then
-    SECRET1=$(head -7 $SECRET | head -1 | awk '{print $4}' | xargs)
-    SECRET2=$(head -7 $SECRET | head -2 | tail -1 | awk '{print $4}' | xargs)
-    SECRET3=$(head -7 $SECRET | head -3 | tail -1 | awk '{print $4}' | xargs)
-    SECRET4=$(head -7 $SECRET | head -4 | tail -1 | awk '{print $4}' | xargs)
-    SECRET5=$(head -7 $SECRET | head -5 | tail -1 | awk '{print $4}' | xargs)
-    ROOTSECRET=$(head -7 $SECRET | tail -1 | awk '{print $4}' | xargs)
+    UNSEAL1=$(head -7 $SECRET | head -1 | awk '{print $4}' | xargs)
+    UNSEAL2=$(head -7 $SECRET | head -2 | tail -1 | awk '{print $4}' | xargs)
+    UNSEAL3=$(head -7 $SECRET | head -3 | tail -1 | awk '{print $4}' | xargs)
+    UNSEAL4=$(head -7 $SECRET | head -4 | tail -1 | awk '{print $4}' | xargs)
+    UNSEAL5=$(head -7 $SECRET | head -5 | tail -1 | awk '{print $4}' | xargs)
+    ROOT_SECRET=$(head -7 $SECRET | tail -1 | awk '{print $4}' | xargs)
   fi
 }
 
@@ -83,13 +80,13 @@ autounseal() {
   echo 'After operator initilization, you need to unseal the vault 3 times'
   echo 'AUTOUNSEAL is set to yes, automatically unsealing. DO NOT DO THIS IN PRODUCTION'
   echo 'First Unseal Attempt...'
-  vault operator unseal "$SECRET1"
+  vault operator unseal "$UNSEAL1"
   echo
   echo 'Second Unseal Attempt...'
-  vault operator unseal "$SECRET3"
+  vault operator unseal "$UNSEAL3"
   echo
   echo 'Final Unseal Attempt...'
-  vault operator unseal "$SECRET5"
+  vault operator unseal "$UNSEAL5"
   echo
 }
 
@@ -97,12 +94,12 @@ autologin() {
   set_secrets
   echo 'Automatically login using the root token.'
   echo 'DO NOT DO THIS IN PRODUCTION'
-  vault login "$ROOTSECRET"
+  vault login "$ROOT_SECRET"
   echo
 }
 
 unseal() {
-  if [[ "$AUTOUNSEAL" == "yes" ]]; then
+  if [[ "$AUTO_UNSEAL" == "yes" ]]; then
     autounseal
   else
     unseal_message
@@ -110,8 +107,8 @@ unseal() {
 }
 
 login() {
-  if [[ "$AUTOLOGIN" == "yes" ]]; then
-    echo 'AUTOLOGIN is set to yes'
+  if [[ "$AUTO_LOGIN" == "yes" ]]; then
+    echo 'AUTO_LOGIN is set to yes'
     autologin
   else
     login_message
@@ -158,42 +155,37 @@ phase2() {
 # phase 3 is when admin and n00b policies are written to vault
 # and mapped out to their respective teams
 #
-
-authn_define_policy_admin_team() {
+authn_define_policy() {
+  # this function checks a policy file and writes it into vault
+  # assignment is done later
+  local POLICY_NAME=$1
+  local POLICY_FILE=$2
   # exit if the policy document is missing
-  [[ ! -e $ADMINPOLICY ]] && exit
-  echo 'Check and write the admin policy document'
-  vault policy fmt $ADMINPOLICY
-  vault policy write admin-policy $ADMINPOLICY
+  [[ ! -e "$POLICY_FILE" ]] && exit
+  echo "Check and write the $POLICY_NAME policy"
+  vault policy fmt "$POLICY_FILE"
+  vault policy write "$POLICY_NAME" "$POLICY_FILE"
   echo
 }
 
-authn_define_policy_admin_n00bs() {
-  # exit if the policy document is missing
-  [[ ! -e $N00BSPOLICY ]] && exit
-  echo 'Check and write the n00bs policy document'
-  vault policy fmt $N00BSPOLICY
-  vault policy write n00bs-policy $N00BSPOLICY
-  echo
-}
-
-authn_map_admin_policy() {
-  echo 'Assign the admin policy to the admin team from the GitHub configured organization'
-  vault write auth/github/map/teams/admin value=default,admin-policy
-  echo
-}
-
-authn_map_n00bs_policy() {
-  echo 'Assign the n00bs policy to the n00bs team from the GitHub configured organization'
-  vault write auth/github/map/teams/n00bs value=default,n00bs-policy
+github_authn_map_policy() {
+  # this function assigns a team's policy
+  local TEAM_NAME=$1
+  local POLICY_NAME=$1
+  echo "Assign the $POLICY_NAME policy to the $TEAM_NAME team from the GitHub configured organization"
+  vault write auth/github/map/teams/"$TEAM_NAME" value=default,"$POLICY_NAME"
   echo
 }
 
 phase3() {
-  authn_define_policy_admin_team
-  authn_define_policy_admin_n00bs
-  authn_map_admin_policy
-  authn_map_n00bs_policy
+  # set a new policy called admin-policy and assign the admin-policy.hcl document
+  authn_define_policy admin-policy "$POLICY_DIR"/admin-policy.hcl
+  # map the policy to the github team named "admin"
+  github_authn_map_policy admin admin-policy
+
+  # same goes for the n00bs
+  authn_define_policy n00bs-policy "$POLICY_DIR"/n00bs-policy.hcl
+  github_authn_map_policy n00bs n00bs-policy
 }
 # end of phase 3
 
@@ -230,10 +222,13 @@ phase4(){
 # phase 5 - AppRole
 #
 
-# define variables for files where roleid and secretid
+# define variables for role names and files where roleid and secretid
 # information will be saved
-APPROLE_ROLEID=".funapproll.roleid"
-APPROLE_SECRETID=".funapproll.secretid"
+
+# admin
+APPROLE_ADMIN_NAME="funapprolladmin"
+# client
+APPROLE_CLIENT_NAME="funapprollclient"
 
 authn_enable_approle() {
   echo 'Enable AppRole authentication'
@@ -242,10 +237,12 @@ authn_enable_approle() {
 }
 
 approle_create_role() {
-  echo 'Create an example role: funapproll'
-  vault write auth/approle/role/funapproll \
+  # this function takes the first argument and assigns it to a local variable ROLE_NAME
+  local ROLE_NAME=$1
+  echo "Create an example application role: $ROLE_NAME"
+  vault write auth/approle/role/$ROLE_NAME \
     secret_id_ttl=10m \
-    token_num_uses=10 \
+    token_num_uses=3 \
     token_ttl=20m \
     token_max_ttl=30m \
     secret_id_num_uses=40
@@ -253,26 +250,43 @@ approle_create_role() {
 }
 
 approle_fetch_roleid() {
-  echo 'Fetch the RoleID for funapproll'
+  # this function takes 2 arguments, the 1st assigned to a local variable ROLE_NAME
+  # the 2nd assignment is for the file where the role-id information is saved
+  local ROLE_NAME="$1"
+  local ROLEID_FILE=".$ROLE_NAME.role_id"
+  echo "Fetching RoleID: $ROLE_NAME"
   # the fetched information is written both to stdout and a file using the tee ulititee
-  vault read auth/approle/role/funapproll/role-id | tee $APPROLE_ROLEID
+  vault read auth/approle/role/$ROLE_NAME/role-id | tee "$ROLE_ID_FILE"
   echo
 }
 
-approle_fetch_secretid() {
-  echo 'Fetch the SecretID for funapproll'
-  vault write -f auth/approle/role/funapproll/secret-id | tee $APPROLE_SECRETID
+approle_fetch_admin_secretid() {
+  # this function takes 2 arguments, the 1st assigned to a local variable ROLE_NAME
+  # the 2nd assignment is for the file where the secret-id information is saved
+  local ROLE_NAME=$1
+  local SECRET_ID_FILE=".$ROLE_NAME.secret_id"
+  echo 'Fetching SecretID: $ROLE_NAME'
+  vault write -f auth/approle/role/$ROLE_NAME/secret-id | tee "$SECRET_ID_FILE"
   echo
 }
 
 phase5() {
-  echo 'Need to re-login w/ the root token to enable AppRole'
+  echo 'Re-login w/ the root token to enable AppRole'
   echo 'vault login <root-token-here>'
   autologin
   authn_enable_approle
-  approle_create_role
-  approle_fetch_roleid
-  approle_fetch_secretid
+  # create the admin role first
+  approle_create_role $APPROLE_ADMIN_NAME
+  # fetch the admin's role-id and save it to a file
+  approle_fetch_roleid $APPROLE_ADMIN_NAME $APPROLE_ADMIN_ROLEID
+  # fetch the admin's secret-id and save it to a file
+  approle_fetch_secretid $APPROLE_ADMIN_NAME $APPROLE_ADMIN_SECRETID
+  # create the client role next
+  approle_create_role $APPROLE_CLIENT_NAME
+  # fetch the client's role-id and save it to a file
+  approle_fetch_roleid $APPROLE_CLIENT_NAME $APPROLE_CLIENT_ROLEID
+  # fetch the client's secret-id and save it to a file
+  approle_fetch_secretid $APPROLE_CLIENT_NAME $APPROLE_CLIENT_SECRETID
 }
 # end of phase 5
 
@@ -280,78 +294,77 @@ phase5() {
 # phase 6 - AppRole and Authorization
 #
 
-# funrollapp policy files definition
-FUNAPPROLL_ADMINPOLICY=$POLICYDIR/funapproll-admin.hcl
-FUNAPPROLL_CLIENTPOLICY=$POLICYDIR/funapproll-client.hcl
-
-# check and write the funapproll polices into vault
-authn_define_policy_funapproll_admin() {
-  # exit if the policy document is missing
-  [[ ! -e $FUNAPPROLL_ADMINPOLICY ]] && exit
-  echo 'Check and write the funapproll admin policy'
-  vault policy fmt $FUNAPPROLL_ADMINPOLICY
-  vault policy write funapprolladmin-policy $FUNAPPROLL_ADMINPOLICY
-  echo
-}
-
-authn_define_policy_funapproll_client() {
-  # exit if the policy document is missing
-  [[ ! -e $FUNAPPROLL_CLIENTPOLICY ]] && exit
-  echo 'Check and write the funapproll client policy'
-  vault policy fmt $FUNAPPROLL_CLIENTPOLICY
-  vault policy write funapprollclient-policy $FUNAPPROLL_CLIENTPOLICY
-  echo
-}
-
 # update the funapproll role policies
-authn_update_funapproll_policy() {
-  echo 'Write the policies for the funapproll role'
-  vault write auth/approle/role/funapproll/policies policies=default,funapprolladmin-policy,funapprollclient-policy
+approle_authn_assign_policy() {
+  local APPROLE_NAME=$1
+  local POLICY_NAME=$2
+  echo "Updates the policies for the $APPROLE_NAME role"
+  vault write auth/approle/role/"$APPROLE_NAME"/policies policies=default,"$POLICY_NAME"
   echo
 }
 
 # check the current list of polices for the funapproll role
-authn_check_funapproll_policy() {
-  echo 'Read attributes of the funapproll,check policies exist'
-  vault read auth/approle/role/funapproll
+approle_authn_check_policy() {
+  local APPROLE_NAME=$1
+  echo "Read attributes of the $APPROLE_NAME, check if the policy list correctness"
+  vault read auth/approle/role/"$APPROLE_NAME"
   echo
 }
 
 phase6() {
-  authn_define_policy_funapproll_admin
-  authn_define_policy_funapproll_client
-  authn_update_funapproll_policy
-  authn_check_funapproll_policy
+  # funrollapp policy files definition
+  APPROLE_ADMIN_POLICY="$POLICY_DIR"/funapproll-admin.hcl
+  APPROLE_CLIENT_POLICY="$POLICY_DIR"/funapproll-client.hcl
+
+  # set admin policy, assign admin policy to admin role then check
+  authn_define_policy "$APPROLE_ADMIN_NAME" "$APPROLE_ADMIN_POLICY"
+  approle_authn_assign_policy "$APPROLE_ADMIN_NAME" "$APPROLE_ADMIN_POLICY"
+  approle_authn_check_policy "$APPROLE_ADMIN_NAME" "$APPROLE_ADMIN_POLICY"
+
+  # do the client next
+  authn_define_policy "$APPROLE_CLIENT_NAME" "$APPROLE_CLIENT_POLICY"
+  approle_authn_assign_policy "$APPROLE_CLIENT_NAME" "$APPROLE_CLIENT_POLICY"
+  approle_authn_check_policy "$APPROLE_CLIENT_NAME" "$APPROLE_CLIENT_POLICY"
 }
 
 #
 # phase 7 - AppRole Test
+# In this phase we simulate an "app" by interacting w/ the vault API
+# using curl. JSON responses are parsed by `jq` and should be installed
 #
-APPROLE_LOGIN_RESPONSE=".funapproll.login.json"
-
 approle_login() {
-  [[ ! -e $APPROLE_ROLEID ]] && exit
-  ROLEID="$(tail -1 $APPROLE_ROLEID | awk '{print $2}' | xargs)"
+  local ROLE_NAME=$1
+  local ROLE_ID_FILE=".$ROLE_NAME.role_id"
+  SECRET_ID_FILE=$2
+  LOGIN_RESPONSE_FILE=3
+  [[ ! -e "$ROLE_ID_FILE" ]] && exit
+  ROLE_ID="$(tail -1 $ROLE_ID_FILE | awk '{print $2}' | xargs)"
 
-  [[ ! -e $APPROLE_SECRETID ]] && exit
-  SECRETID="$( tail -2 $APPROLE_SECRETID | head -1 | awk '{print $2}' | xargs)"
+  [[ ! -e "$SECRET_ID_FILE" ]] && exit
+  SECRET_ID="$( tail -2 $SECRET_ID_FILE | head -1 | awk '{print $2}' | xargs)"
 
-  # authentiction to vault using funapproll role
-  echo 'Authenticating to vault using funapproll role_id and secret_id'
-  curl \
+  # authentiction to vault using role
+  echo 'Authenticating to vault using role_id and secret_id'
+  curl -s \
     --request POST \
-    --data '{"role_id":"'$ROLEID'","secret_id":"'$SECRETID'"}' \
-    http://127.0.0.1:8200/v1/auth/approle/login | tee "$APPROLE_LOGIN_RESPONSE"
+    --data '{"role_id":"'$ROLE_ID'","secret_id":"'$SECRET_ID'"}' \
+    http://127.0.0.1:8200/v1/auth/approle/login &> "$LOGIN_RESPONSE_FILE"
+  cat "$LOGIN_RESPONSE_FILE" | jq '.'
   echo
 }
 
 approle_read_login() {
-  cat "$APPROLE_LOGIN_RESPONSE" | jq '. | {client_token: .auth.client_token}'
+  LOGIN_RESPONSE_FILE=$1
+  CLIENT_TOKEN_FILE=$2
+  cat "$LOGIN_RESPONSE_FILE" | jq '. | .auth.client_token' | xargs | tee $CLIENT_TOKEN_FILE
 }
 
 phase7() {
-  # secret ids have ttl, so we need to re-fetch
-  approle_fetch_secretid
+  local APPROLE_ADMIN_LOGIN_RESPONSE_FILE=".funapprolladmin-login.json"
+  local APPROLE_ADMIN_CLIENT_TOKEN_FILE=".funapprolladmin.token"
+  local
+  # secret ids have ttl, so we need to re-fetch the secret_id
+  approle_fetch_secretid "$APPROLE_ADMIN_NAME" "$APPROLE_ADMIN_SECRETID"
   approle_login
   approle_read_login
 }
@@ -361,13 +374,13 @@ phase7() {
 # helper functions
 #
 stop_consul_dev() {
-  kill -9 $(cat $CONSULPID)
-  [[ -e $CONSULPID ]] && rm $CONSULPID
+  kill -9 $(cat "$CONSUL_PID")
+  [[ -e "$CONSUL_PID" ]] && rm "$CONSUL_PID"
 }
 
 stop_vault() {
-  kill -9 $(cat $VAULTPID)
-  [[ -e $VAULTPID ]] && rm $VAULTPID
+  kill -9 $(cat "$VAULT_PID")
+  [[ -e "$VAULT_PID" ]] && rm "$VAULT_PID"
 }
 
 stop() {
